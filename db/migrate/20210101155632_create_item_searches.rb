06e2e7e1cfb21943aeccfdc4d05ff04530b2a1f1
misakitsubota <misakitsubota@icloud.com>
2021-01-02 17:19:03 +0900

    commentモデル作成

--- db/migrate/20210101155632_create_item_searches.rb
+++
@@ -1,9 +0,0 @@
-class CreateItemSearches < ActiveRecord::Migration[6.0]
-  def change
-    create_table :item_searches do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
