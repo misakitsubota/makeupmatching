eb24016429be325db77d1061e98613e86cf6a4ce
misakitsubota <misakitsubota@icloud.com>
2021-01-02 17:25:10 +0900

    migration実行

--- db/migrate/20210102081640_create_comments.rb
+++ db/migrate/20210102081640_create_comments.rb
@@ -1,7 +1,13 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.text :image
+      t.string :recommend_item_name, null: false
+      t.integer :cost, null: false
+      t.text :description, null:false
+      t.integer :user_id, null: false
+      t.integer :brand_id, null: false
+      t.integer :category_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_155632) do
+ActiveRecord::Schema.define(version: 2021_01_02_081640) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,8 +33,14 @@ ActiveRecord::Schema.define(version: 2021_01_01_155632) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "item_searches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image"
+    t.string "recommend_item_name", null: false
+    t.integer "cost", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.integer "brand_id", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
