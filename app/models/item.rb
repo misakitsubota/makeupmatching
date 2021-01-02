d12cc59eea99e86a3f9e207765c8f8e46ceb6804
misakitsubota <misakitsubota@icloud.com>
2021-01-02 17:32:53 +0900

    アソシエーション・ルーティング記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_many :comments
   belongs_to :brand
   belongs_to :category
-  belongs_to :item_search
   has_one_attached :image
 
   #空の投稿を保存できないようにする
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
   get 'items/search', to: 'items#search'
-  resources :items
+  resources :items do
+    resources :comments, only: :create
+  end
 end
